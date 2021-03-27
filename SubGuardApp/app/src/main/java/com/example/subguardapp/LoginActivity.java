8f9f7ec5affe934ddb3fbf11abdcf1bb9bf97994
notnamenot <izabela.pachel@wp.pl>
2021-03-27 22:08:34 +0100

    login LoginActivity -> navigationDrawer

--- SubGuardApp/app/src/main/java/com/example/subguardapp/LoginActivity.java
+++ SubGuardApp/app/src/main/java/com/example/subguardapp/LoginActivity.java
@@ -2,8 +2,15 @@ package com.example.subguardapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -14,6 +21,23 @@ public class LoginActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 //        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 //        this.supportActionBar?.hide();
-        
+
+        Button login = (Button) findViewById(R.id.tvLogin);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                if ("Boiling Point K".equals(login.getText().toString()))
+//                    login.setText("2792");
+//                else
+//                    login.setText("Boiling Point K");
+                Intent intent = new Intent(LoginActivity.this, navigation_drawer.class);
+//                EditText editText = (EditText) findViewById(R.id.editText);
+//                String message = editText.getText().toString();
+//                intent.putExtra(EXTRA_MESSAGE, message);
+                startActivity(intent);
+
+            }
+        });
     }
 }
