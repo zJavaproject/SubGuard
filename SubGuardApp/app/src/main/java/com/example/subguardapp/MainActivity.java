2f983ccedb0ec62d32d3345579713f44a21cf58a
Ada <k.adrianna@interia.eu>
2021-03-28 08:22:58 +0200

    push notification

--- SubGuardApp/app/src/main/java/com/example/subguardapp/MainActivity.java
+++ SubGuardApp/app/src/main/java/com/example/subguardapp/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.subguardapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
 }
