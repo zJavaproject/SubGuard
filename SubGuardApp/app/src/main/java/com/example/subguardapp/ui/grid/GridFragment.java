17be64502eb9928e65fedd4df57d821b973f42c2
Katarzyna Pencak <katarzyna.pencak23@gmail.com>
2021-03-28 08:04:27 +0200

    Added mock grid

--- SubGuardApp/app/src/main/java/com/example/subguardapp/ui/grid/GridFragment.java
+++ SubGuardApp/app/src/main/java/com/example/subguardapp/ui/grid/GridFragment.java
@@ -1,56 +1,24 @@
 package com.example.subguardapp.ui.grid;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.TextView;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.example.subguardapp.ui.details.MyItemRecyclerViewAdapter;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import com.example.subguardapp.R;
-import com.example.subguardapp.dummy.ListItemContent;
 
 public class GridFragment extends Fragment {
 
-    private static final String ARG_COLUMN_COUNT = "column-count";
-    private int mColumnCount = 3;
-
-//    private DetailViewModel detailsViewModel;
-
-    public GridFragment() {
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        if (getArguments() != null) {
-            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
-        }
-    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-//        detailsViewModel =
-//                new ViewModelProvider(this).get(DetailViewModel.class);
-        View root = inflater.inflate(R.layout.grid, container, false);
 
-        if (root instanceof RecyclerView) {
-            Context context = root.getContext();
-            RecyclerView recyclerView = (RecyclerView) root;
-            if (mColumnCount <= 1) {
-                recyclerView.setLayoutManager(new LinearLayoutManager(context));
-            } else {
-                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
-            }
-            recyclerView.setAdapter(new MyItemRecyclerViewAdapter(ListItemContent.ITEMS));
-        }
+        View root = inflater.inflate(R.layout.grid_mock, container, false);
         return root;
     }
 }
