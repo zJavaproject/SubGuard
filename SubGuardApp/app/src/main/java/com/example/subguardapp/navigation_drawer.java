a63c3f8368995648dcf7ab39cedc88504535ba98
notnamenot <izabela.pachel@wp.pl>
2021-03-28 05:12:42 +0200

    example user

--- SubGuardApp/app/src/main/java/com/example/subguardapp/navigation_drawer.java
+++ SubGuardApp/app/src/main/java/com/example/subguardapp/navigation_drawer.java
@@ -24,7 +24,8 @@ public class navigation_drawer extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation_drawer);
         Toolbar toolbar = findViewById(R.id.toolbar);
-        toolbar.setNavigationIcon(R.drawable.sub_guard_logo_solo);
+//        toolbar.setNavigationIcon(R.drawable.sub_guard_logo_solo);
+//        toolbar.setLogo(R.drawable.sub_guard_logo_solo);
         setSupportActionBar(toolbar);
 //        toolbar.setOverflowIcon(ContextCompat.getDrawable(this,R.drawable.sub_guard_logo_solo));
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -36,11 +37,12 @@ public class navigation_drawer extends AppCompatActivity {
             }
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
+
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_details, R.id.nav_slideshow, R.id.nav_grid )
+                R.id.nav_home, R.id.nav_details, R.id.nav_logout, R.id.nav_grid )
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
--- SubGuardApp/app/src/main/java/com/example/subguardapp/ui/logout/LogoutViewModel.java
+++ SubGuardApp/app/src/main/java/com/example/subguardapp/ui/logout/LogoutViewModel.java
@@ -1,16 +1,16 @@
-package com.example.subguardapp.ui.slideshow;
+package com.example.subguardapp.ui.logout;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class SlideshowViewModel extends ViewModel {
+public class LogoutViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public SlideshowViewModel() {
+    public LogoutViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is slideshow fragment");
+        mText.setValue("This is logout fragment");
     }
 
     public LiveData<String> getText() {
