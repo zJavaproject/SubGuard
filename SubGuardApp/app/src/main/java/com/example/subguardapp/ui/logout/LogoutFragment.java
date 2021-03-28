4d59fe1f6dd648a0d197d6f93093cfa5ead40f24
notnamenot <izabela.pachel@wp.pl>
2021-03-28 07:19:09 +0200

    login back

--- SubGuardApp/app/src/main/java/com/example/subguardapp/ui/logout/LogoutFragment.java
+++ SubGuardApp/app/src/main/java/com/example/subguardapp/ui/logout/LogoutFragment.java
@@ -1,5 +1,6 @@
 package com.example.subguardapp.ui.logout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,7 +11,10 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+
+import com.example.subguardapp.LoginActivity;
 import com.example.subguardapp.R;
+import com.example.subguardapp.navigation_drawer;
 
 public class LogoutFragment extends Fragment {
 
@@ -18,6 +22,13 @@ public class LogoutFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
             ViewGroup container, Bundle savedInstanceState) {
+
+        Intent intent = new Intent(getActivity(), LoginActivity.class);
+//                EditText editText = (EditText) findViewById(R.id.editText);
+//                String message = editText.getText().toString();
+//                intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
         logoutViewModel =
                 new ViewModelProvider(this).get(LogoutViewModel.class);
         View root = inflater.inflate(R.layout.fragment_logout, container, false);
